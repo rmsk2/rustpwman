c6bb28df26297d4cb0a221bfd06f83d89a578757
Martin <rmsk2@gmx.de>
2021-07-01 21:32:16 +0200

    Begin to implement encrypt and decrypt commands

--- src/main.rs
+++ src/main.rs
@@ -20,16 +20,101 @@ mod jots;
 mod pwgen;
 mod modtui;
 
+use clap::{Arg, App, SubCommand};
+
+const COMMAND_ENCRYPT: &str = "enc";
+const COMMAND_DECRYPT: &str = "dec";
+const COMMAND_GUI: &str = "gui";
+const ARG_INPUT_FILE: &str = "inputfile";
+const ARG_OUTPUT_FILE: &str = "outputfile";
+const ARG_SCRYPT: &str = "scrypt";
+
 fn main() {
-    let args: Vec<String> = env::args().skip(1).collect();
+    let mut app = App::new("rustpwman")
+        .version("0.5.5")
+        .author("Martin Grap <rmsk2@gmx.de>")
+        .about("A password manager for the cursive TUI in Rust")          
+        .subcommand(
+            SubCommand::with_name(COMMAND_ENCRYPT)
+                .about("Encrypt file")        
+                .arg(Arg::with_name(ARG_INPUT_FILE)
+                    .short("i")
+                    .long("input")
+                    .takes_value(true)
+                    .required(true)
+                    .help("Name of plaintext file to encrypt"))
+                .arg(Arg::with_name(ARG_OUTPUT_FILE)
+                    .short("o")
+                    .long("output")
+                    .required(true)
+                    .takes_value(true)
+                    .help("Encrypted output file"))                    
+                .arg(Arg::with_name(ARG_SCRYPT)
+                    .long("scrypt")
+                    .help("Use Scrypt as PBKDF")))
+        .subcommand(
+            SubCommand::with_name(COMMAND_DECRYPT)
+                .about("Decrypt file")        
+                .arg(Arg::with_name(ARG_INPUT_FILE)
+                    .short("i")
+                    .long("input")
+                    .required(true)
+                    .takes_value(true)
+                    .help("Name of encrypted file"))
+                .arg(Arg::with_name(ARG_OUTPUT_FILE)
+                    .short("o")
+                    .long("output")
+                    .required(true)
+                    .takes_value(true)
+                    .help("Name of plaintext file"))                    
+                .arg(Arg::with_name(ARG_SCRYPT)
+                    .long("scrypt")
+                    .help("Use Scrypt as PBKDF")))
+        .subcommand(
+            SubCommand::with_name(COMMAND_GUI)
+                .about("Open file in TUI")        
+                .arg(Arg::with_name(ARG_INPUT_FILE)
+                    .short("i")
+                    .long("input")
+                    .required(true)
+                    .takes_value(true)
+                    .help("Name of encrypted data file"))                   
+                .arg(Arg::with_name(ARG_SCRYPT)
+                    .long("scrypt")
+                    .help("Use Scrypt as PBKDF")));                    
 
-    if args.len() < 1 {
-        println!("Usage: rustpwman <encrypted data file>");
-        return;
-    }
+    let matches = app.clone().get_matches();
+    let subcommand = matches.subcommand();
 
-    let data_file_name = args[0].clone();
-    let default_sec_bits = modtui::AppState::determine_sec_level();
+    match subcommand {
+        (COMMAND_ENCRYPT, Some(encrypt_matches)) => {
+            ()
+        },
+        (COMMAND_DECRYPT, Some(decrypt_matches)) => {
+            ()
+        },
+        (COMMAND_GUI, Some(gui_matches)) => {
+            let mut derive: fcrypt::KeyDeriver = fcrypt::GcmContext::sha256_deriver;
 
-    modtui::main_gui(data_file_name, default_sec_bits, fcrypt::GcmContext::scrypt_deriver);
+            if gui_matches.is_present(ARG_SCRYPT) {
+                derive = fcrypt::GcmContext::scrypt_deriver;
+            }
+
+            let mut file_names: Vec<String> = Vec::new();
+            if let Some(in_files) = gui_matches.values_of(ARG_INPUT_FILE) {
+                in_files.for_each(|x| file_names.push(String::from(x)));
+            }
+
+            let data_file_name = file_names[0].clone();
+            let default_sec_bits = modtui::AppState::determine_sec_level();
+        
+            modtui::main_gui(data_file_name, default_sec_bits, derive);
+        },        
+        _ => {
+            match app.print_long_help() {
+                Err(e) => eprintln!("{}", e),
+                _ => eprintln!("")
+            }
+        }
+    };
 }
