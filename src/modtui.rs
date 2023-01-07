0aaeca8336d2317af152cdec247c34692007c2df
Martin Grap <rmsk2@gmx.de>
2023-01-07 17:52:18 +0100

    Cosmetics

--- src/modtui.rs
+++ src/modtui.rs
@@ -885,7 +885,7 @@ fn main_window(s: &mut Cursive, state: AppState, sndr: Rc<Sender<String>>) {
         })
         .delimiter()
         .leaf("About ...", |s| {
-            let msg_str = format!("\n   A basic password manager\n\nWritten by Martin Grap in 2021/2022\n\n        Version {}", VERSION_STRING);
+            let msg_str = format!("\n   A basic password manager\n\nWritten by Martin Grap in 2021-2023\n\n        Version {}", VERSION_STRING);
             show_message(s, &msg_str[..]);
         })            
         .delimiter()
