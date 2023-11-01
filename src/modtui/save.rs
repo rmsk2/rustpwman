3dac15c1c2bb2d0c4265bfc1ee8c1a5ccb2c02af
Martin Grap <rmsk2@gmx.de>
2023-11-01 11:26:53 +0100

    Remove unneccessary test

--- src/modtui/save.rs
+++ src/modtui/save.rs
@@ -8,18 +8,15 @@ use super::show_message;
 use super::visualize_if_modified;
 
 pub fn storage(s: &mut Cursive, state_temp_save: Rc<RefCell<AppState>>) {
-    match state_temp_save.borrow().password.clone() {
-        Some(_) => (),
-        None => { show_message(s, "Unable to read password"); return; }
-    };
-
+    // force release of mutable reference to state_temp_save before
+    // calling visualize_if_modified
     {
         let mut mut_state = state_temp_save.borrow_mut();
-    
+
         if let Err(e) = mut_state.persist_store() {
             show_message(s, &format!("Unable to save: {:?}", e)); 
             return; 
-        }    
+        }        
     }
 
     visualize_if_modified(s, state_temp_save.clone());
