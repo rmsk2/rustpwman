39f53244c618b40378521bac1a10cf646975006a
Martin Grap <rmsk2@gmx.de>
2023-12-10 17:16:42 +0100

    Add license headers

--- src/fcrypt/chacha20.rs
+++ src/fcrypt/chacha20.rs
@@ -12,6 +12,7 @@ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 See the License for the specific language governing permissions and
 limitations under the License. */
 
+
 use std::io::Read;
 use std::io::Write;
 use crate::fcrypt::{Cryptor, AeadContext, KdfId, KeyDeriver};
